6e5044c6979913b201129031eb5b67d8719d7078
Erno Lokkila <eolokk@utu.fi>
2022-07-04 17:39:52 +0300

    Added in-game timer

---
+++ Clock.py
@@ -0,0 +1,44 @@
+import threading
+import time
+
+class Timer(threading.Thread):
+
+    ticks = 0
+    observers = []
+    
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.ticks = 0
+        self.observers = []        
+        
+    def run(self):       
+        print ("Starting " + self.name)
+        self.running = True
+        while(self.running):
+            time.sleep(1)
+            self.tick()
+        print ("Stopping " + self.name)
+        
+    def tick(self):
+        self.ticks += 1
+        for o in self.observers:
+            o(self.ticks)
+
+    def getTick(self):
+        return self.ticks
+
+    def registerObserver(self, observer):
+        self.observers.append(observer)
+
+    def removeObserver(self, observer):
+        self.observers.remove(observer)
+
+    def startListening(self):
+        self.running = True
+        self.start()
+
+    def stopListening(self):
+        self.running = False
+#python Unityprojects\textbasedjam2022\MainGame.py
